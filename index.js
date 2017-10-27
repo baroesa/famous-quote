7f14635b7f0ba990a68c548c80d6e856586502b3
baroesa <baroesa@gmail.com>
2017-10-27 08:32:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,95 @@
+//Rendome Quote word
+var comments = ['`TB Joshua`: `If you don’t have a vision, you can never be focused in a direction.`', '`TB Joshua` : `Every day, God has something to say to you. Even when He seems to be quiet, God is still saying something.`', '`TB Joshua` : `Where there is unconditional love, the wound of one is the wound of all.`','`TB Joshua` : `Your hands may be full of money and your brain full of information but if your heart is empty, your life is very empty.`','`TB Joshua` : `If you turn to God once, He will turn to you a million times.`','`TB Joshua` : `No matter how fast a lie runs, the truth will someday overtake it.`','`TB Joshua` : `Life is too short to waste time on things that have no lasting significance.`','`TB Joshua` : `There is no one and there will never be anyone like you.`','`TB Joshua` : `Everyone makes mistakes – mistakes are correctable.`','`TB Joshua` : `Avoid the trap of looking back unless it is to glorify God for what He has done.`','`TB Joshua` : `Education is not success but it is to help us succeed.`','`TB Joshua` : `You are responsible for what you give your attention to.`','`TB Joshua` : `Your situation does not embarrass God because He has solution.`','`TB Joshua` : `If you are in a hurry to get money, you will be in a hurry to spend it. Anything that starts dubiously will surely end dubiously.`','`Seneca` : `Not how long, but how well you have lived is the main thing.`','`John Lennon` : `Life is what happens when you’re busy making other plans.`','`Charles Swindoll` : `Life is ten percent what happens to you and ninety percent how you respond to it.`','`Winston Churchill` : `Keep calm and carry on.`','`Forrest Gump` : `Life is like a box of chocolates. You never know what you’re going to get.`','`Mahatma Ghandi` : `Where there is love there is life.`','`Stephen King` : `Get busy living or get busy dying.`','`Henry David Thoreau` : `Go confidently in the direction of your dreams! Live the life you’ve imagined.`','`Jack Kerouac` : `Maybe that’s what life is… a wink of the eye and winking stars.`','`Jonathan Swift` : `May you live all the days of your life.`','`Benjamin Franklin` : `Dost thou love life? Then do not squander time, for that is the stuff life is made of.`','`Nathan Hale` : `I only regret that I have but one life to give for my country.`','`Helen Keller` : `Life is either a daring adventure or nothing at all.`','`Victor Hugo` : `Life is a flower of which love is the honey.`','`Mae West` : `You only live once, but if you do it right, once is enough.`','`Robert Frost` : `In three words I can sum up everything I’ve learned about life: It goes on.`','`Albert Einstein` : `Only a life lived for others is a life worthwhile.`','`Ray Bradbury` : `Life is trying things to see if they work.`','`Marilyn Monroe` : `Keep smiling, because life is a beautiful thing and there’s so much to smile about.`','`Nelson Mandela` : `The greatest glory in living lies not in never falling, but in rising every time we fall.`','`Mother Teresa` : `In this life we cannot do great things. We can only do small things with great love.`','`Frank Sinatra` : `The big lesson in life, baby, is never be scared of anyone or anything.`','`William Shakespeare` : `Love all, trust a few, do wrong to none.`','`Buddha` : `Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.`','`Dr. Seuss` : `You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.`','`Anthony Robbins` : `The only impossible journey is the one you never begin.`','`D. H. Lawrence` : `Life is ours to be spent, not to be saved.`','`Bob Marley` : `Love the life you live. Live the life you love.`','`Mark Twain` : `Good friends, good books, and a sleepy conscience: this is the ideal life.`','`Edgar Allan Poe` : `All that we see and seem is but a dream within a dream.`','`John Wooden` : `Do not let making a living prevent you from making a life.`','`Babe Ruth` : `Never let the fear of striking out keep you from playing the game.`','`Maya Angelou` : `You will face many defeats in life, but never let yourself be defeated.`','`Confucius` : `Life is really simple, but we insist on making it complicated.`','`James M. Barrie` : `Life is a long lesson in humility.`','`Ernest Hemingway` : `In order to write about life first you must live it.`','`Dalai Lama` : `The purpose of our lives is to be happy.`','`Tom Cochrane` : `Life is a highway. I want to ride it all night long.`','`Hans Christian Andersen` : `Life itself is the most wonderful fairy tale.`','`Stephen Hawking` : `Life would be tragic if it weren’t funny.`','`Ralph Waldo Emerson` : `Live in the sunshine, swim the sea, drink the wild air.`','`Euripides` : `The greatest pleasure of life is love.`','`Oscar Wilde` : `Life is never fair, and perhaps it is a good thing for most of us that it is not.`','`George Sand` : `There is only one happiness in life – to love and to be loved.`','`Grandma Moses` : `Life is what we make it, always has been, always will be.`','`Benjamin Franklin` : `Life’s tragedy is that we get old too soon and wise too late.`','`Eleanor Roosevelt` : `If life were predictable it would cease to be life, and be without flavor.`','`George Bernard Shaw` : `Life isn’t about finding yourself. Life is about creating yourself.`','`Aristotle` : `The energy of the mind is the essence of life.`','`Søren Kierkegaard` : `Life is not a problem to be solved, but a reality to be experienced.`','`Ralph Waldo Emerson` : `Life is a succession of lessons which must be lived to be understood.`','`Charles Dickens` : `Life is made of ever so many partings welded together.`','`Audrey Hepburn` : `The most important thing is to enjoy your life — to be happy — it’s all that matters.`','`Abraham Lincoln` : `In the end, it’s not the years in your life that count. It’s the life in your years.`','`T. S. Eliot` : `The dream crossed twilight between birth and dying.`','`Kevin Kruse` : `Life is about making an impact, not making an income.`','`Napoleon Hill` : `Whatever the mind of man can conceive and believe, it can achieve.`','`Albert Einstein` : `Strive not to be a success, but rather to be of value.`','`Robert Frost` : `Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.`','`Florence Nightingale` : `I attribute my success to this: I never gave or took any excuse.`','`Wayne Gretzky` : `You miss 100% of the shots you don’t take.`','`Amelia Earhart` : `The most difficult thing is the decision to act, the rest is merely tenacity.`','`Babe Ruth` : `Every strike brings me closer to the next home run.`','`W. Clement Stone` : `Definiteness of purpose is the starting point of all achievement.`','`John Lennon` : `Life is what happens to you while you’re busy making other plans.`','`Earl Nightingale` : `We become what we think about.`','`Mark Twain` : `Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.`','`Charles Swindoll` : `Life is 10% what happens to me and 90% of how I react to it.`','`Alice Walker` : `The most common way people give up their power is by thinking they don’t have any.`','`Buddha` : `The mind is everything. What you think you become.`','`Chinese Proverb` : `The best time to plant a tree was 20 years ago. The second best time is now.`','`Socrates` : `An unexamined life is not worth living.`'];   
+
+//console.log(item);
+
+// Steemit Private Posting Key
+var wifkey = '';
+
+// Steemit username
+var votey = "";
+
+// Enter the Tag you want to upvote new posts in
+var targettag = "";
+        
+// May want to modify this (10000 = 100% vote)
+var weight = 10000;
+
+// No need to modify these variables
+var steem = require('steem');
+var totalvote = 0;
+var metadatascan;
+var json_metadata;
+var op;
+
+steem.api.streamBlockNumber(function (err1, newestblock) {
+    console.log("Scanning Block #" + newestblock + " For New #" + targettag + " Posts - Posts Voted: " + totalvote);
+});
+
+
+var process_post = function (op) {
+    if (op["author"] != "") {
+      // console.log(targettag + " Post Has Been Detected!");
+        steem.broadcast.vote(
+            wifkey,
+            votey,
+            op["author"],
+            op["permlink"],
+            weight,
+            function (downerr, result) {
+                if (downerr) {
+                    var error = JSON.stringify(downerr);
+                    if (error.toLowerCase().indexOf("You have already voted\n") >= 0) {
+                        console.log("Oops! a post it already voted on!");
+                    }
+                }
+                if (result) {
+                    totalvote++;
+                    console.log("Successfully voted #" + targettag + " post!");
+					//-------------------comment
+                var time3 = new Date() /1;
+                var item = comments[(Math.random()*comments.length)|0];
+				var permlink1 = 're-'+op["author"]+'-'+op["permlink"]+'-'+time3+'z';
+				var jsonMetadata = '{"tags":["steem"],"app":"comment.js/0.01"}';
+                steem.broadcast.comment(wifkey, op["author"], op["permlink"], votey, permlink1, '', '#life\n '+item , jsonMetadata, function(err, result) {});
+                				//-------------------------up comment
+				const json = JSON.stringify(['reblog', {
+                    account: votey,
+                    author: op["author"],
+                    permlink: op["permlink"]
+                  }]);
+                  
+                  steem.broadcast.customJson(wifkey, [], [votey], 'follow', json, (err, result) => {
+                  //  console.log(err, result);
+                  });
+                  //--------------resteem
+				
+				}
+            }
+        );
+			
+    }
+};
+//----- Streeming Latest Block Operations
+steem.api.streamOperations(function (err2, blockops) {
+    var opType = blockops[0];
+    var op = blockops[1];
+    if (op["json_metadata"] != undefined) {
+
+        metadatascan = op["json_metadata"];
+        if (metadatascan != '') {
+            var tags = JSON.parse(metadatascan);
+            var actualtags = tags["tags"];
+            if (actualtags != undefined) {
+                var tagtag = String(actualtags);
+
+                if (op["parent_author"] === '') {
+                    if (tagtag.toLowerCase().indexOf(targettag) >= 0) {
+                        process_post(op);
+                    }
+                }
+            }
+
+        }
+    }
+});
